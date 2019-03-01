794119a01fa90b04a8d7f82a623073603d6a9104
Moiz Rizvi <moizrizvi@gmail.com>
2019-02-28 18:19:56 -0600

    [Fix] BookCard height to 100

--- src/components/BookCard/index.js
+++ src/components/BookCard/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { Card } from 'antd';
 
 const BookCard = ({ book }) => (
-  <Card style={{ margin: 5, textAlign: 'center' }}>
+  <Card style={{ margin: 5, textAlign: 'center', height: 100 }}>
     <b>{book.Title}</b>
     <br/>
     {book.Author}
